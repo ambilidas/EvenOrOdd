268050a24a3d046cd655debc5b214ff22af260ca
Ambili das v s <ambili.kan@gmail.com>
2022-05-06 23:09:31 +0530

    Program for checking a number is even or odd

---
+++ EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class EvenOdd
+    {
+        public  void checkEvenOrOdd()
+        {
+            Console.WriteLine("Enter a number");
+            int n=Convert.ToInt32(Console.ReadLine());
+            if(n%2 == 0)
+            {
+                Console.WriteLine(n + " is an even number ");
+            }
+            else
+            {
+                Console.WriteLine(n + " is an odd number");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenOdd myobj = new EvenOdd();
+            myobj.checkEvenOrOdd();
+        }
+    }
+}
